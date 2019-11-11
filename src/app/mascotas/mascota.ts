834b79b64d0afdfe5fd1eac7bd0ae933d914e72f
mquiroz25 <mquiroz2535@gmail.com>
2019-11-11 16:58:14 -0300

    tarea 4

---
+++ src/app/mascotas/mascota.ts
@@ -0,0 +1,12 @@
+export interface Mascota {
+
+    
+    id:number;
+    nombre:string;
+    tipo:string;
+    edad:number;
+    descripcion:string;
+
+    
+
+}
---
+++ src/app/mascotas/mascotas.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Mascota } from './mascota';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MascotasService {
+
+  private baseUrl:string = 'http://localhost:8090/mascotas';
+
+  constructor(private httpClient: HttpClient) { }
+
+  
+  public getMascotas(){
+    return this.httpClient.get<Mascota[]>(`${this.baseUrl}`);
+    }
+}
